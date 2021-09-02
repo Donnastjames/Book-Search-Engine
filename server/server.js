f530f5da0c300065d035bd44896c1ae94350d4b3
Donna <donnastjames@comcast.net>
2021-09-01 21:43:14 -0700

    server.js to also show where GraphQL server playground URL is

--- server/server.js
+++ server/server.js
@@ -24,10 +24,14 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(routes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// TODO: Put this back !!!!!!!!
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
